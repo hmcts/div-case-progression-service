22fa09e3b5f3b480165e5ee2f71d22bcad4a53a7
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-04-14 17:51:03 +0100

    Hardcoded the keys azure read is not working

--- automation-test/src/test/java/uk/gov/hmcts/reform/divorce/auth/config/ServiceContextConfiguration.java
+++ automation-test/src/test/java/uk/gov/hmcts/reform/divorce/auth/config/ServiceContextConfiguration.java
@@ -29,9 +29,6 @@ public class ServiceContextConfiguration {
             @Value("${case.progression.auth.microservice}") final String microService,
             final ServiceAuthorisationApi serviceAuthorisationApi
     ) {
-        System.out.println("secret : " + secret);
-        System.out.println("microService : " + microService);
-
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, microService, serviceAuthorisationApi);
     }
 }
