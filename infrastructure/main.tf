ab30870d4984e7af95cdd04de0b74e1ab6f9bf9b
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-04-20 12:47:53 +0100

    azure key vault name change

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,7 +20,7 @@ module "div-case-progression" {
         REFORM_ENVIRONMENT = "${var.env}"
         AUTH_PROVIDER_SERVICE_CLIENT_BASEURL = "${var.auth_provider_service_client_baseurl}"
         AUTH_PROVIDER_SERVICE_CLIENT_MICROSERVICE = "${var.auth_provider_service_client_microservice}"
-        AUTH_PROVIDER_SERVICE_CLIENT_KEY = "${data.vault_generic_secret.ccd_submission_s2s_auth_secret.data["value"]}"
+        AUTH_PROVIDER_SERVICE_CLIENT_KEY = "${data.vault_generic_secret.ccd-submission-s2s-auth-secret.data["value"]}"
         AUTH_PROVIDER_SERVICE_CLIENT_TOKENTIMETOLIVEINSECONDS = "${var.auth_provider_service_client_tokentimetoliveinseconds}"
         AUTH_PROVIDER_HEALTH_URI = "${var.auth_provider_service_client_baseurl}/health"
         CCD_CASEDATASTORE_BASEURL = "${local.ccd_casedatastore_baseurl}"
@@ -32,10 +32,10 @@ module "div-case-progression" {
         LOGGING_LEVEL_UK_GOV_HMCTS_CCD = "${var.logging_level_uk_gov_hmcts_ccd}"
         PDF_GENERATOR_BASE_URL = "${local.pdf_generator_base_url}"
         PDF_GENERATOR_HEALTHURL = "${local.pdf_generator_base_url}/health"
-        DRAFT_STORE_API_ENCRYPTION_KEY = "${data.vault_generic_secret.draft_store_api_encryption_key.data["value"]}"
+        DRAFT_STORE_API_ENCRYPTION_KEY = "${data.vault_generic_secret.draft-store-api-encryption-key.data["value"]}"
         DRAFT_STORE_API_BASEURL = "${var.draft_store_api_baseurl}"
         DRAFT_STORE_API_HEALTH_URI = "${var.draft_store_api_baseurl}/health"
-        UK_GOV_NOTIFY_API_KEY = "${data.vault_generic_secret.uk_gov_notify_api_key.data["value"]}"
+        UK_GOV_NOTIFY_API_KEY = "${data.vault_generic_secret.uk-gov-notify-api-key.data["value"]}"
         UK_GOV_NOTIFY_EMAIL_TEMPLATES = "${var.uk_gov_notify_email_templates}"
         DOCUMENT_MANAGEMENT_STORE_URL = "${local.dm_store_url}"
     }
@@ -57,26 +57,26 @@ module "key-vault" {
     product_group_object_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
 }
 
-data "vault_generic_secret" "ccd_submission_s2s_auth_secret" {
+data "vault_generic_secret" "ccd-submission-s2s-auth-secret" {
     path = "secret/${var.vault_env}/ccidam/service-auth-provider/api/microservice-keys/divorceDocumentGenerator"
 }
 
-data "vault_generic_secret" "draft_store_api_encryption_key" {
+data "vault_generic_secret" "draft-store-api-encryption-key" {
     path = "secret/${var.vault_env}/divorce/draft/encryption_key"
 }
 
-data "vault_generic_secret" "uk_gov_notify_api_key" {
+data "vault_generic_secret" "uk-gov-notify-api-key" {
     path = "secret/${var.vault_env}/divorce/notify/api_key"
 }
 
-resource "azurerm_key_vault_secret" "ccd_submission_s2s_auth_secret" {
-    name      = "ccd_submission_s2s_auth_secret"
-    value     = "${data.vault_generic_secret.ccd_submission_s2s_auth_secret.data["value"]}"
+resource "azurerm_key_vault_secret" "ccd-submission-s2s-auth-secret" {
+    name      = "ccd-submission-s2s-auth-secret"
+    value     = "${data.vault_generic_secret.ccd-submission-s2s-auth-secret.data["value"]}"
     vault_uri = "${module.key-vault.key_vault_uri}"
 }
 
-resource "azurerm_key_vault_secret" "draft_store_api_encryption_key" {
-    name      = "draft_store_api_encryption_key"
-    value     = "${data.vault_generic_secret.draft_store_api_encryption_key.data["value"]}"
+resource "azurerm_key_vault_secret" "draft-store-api-encryption-key" {
+    name      = "draft-store-api-encryption-key"
+    value     = "${data.vault_generic_secret.draft-store-api-encryption-key.data["value"]}"
     vault_uri = "${module.key-vault.key_vault_uri}"
 }
