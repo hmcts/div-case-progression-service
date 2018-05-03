dbae0737f01be59ec2eabdc41e98d521c4ebea85
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-05-03 14:25:15 +0100

    disable second service auth healthcheck happening inside the service auth lib

--- src/main/java/uk/gov/hmcts/reform/divorce/CaseProgressionApplication.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/CaseProgressionApplication.java
@@ -10,12 +10,13 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
 import uk.gov.hmcts.reform.divorce.health.CustomHealthAggregator;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ServiceAuthHealthIndicator.class})
 public class CaseProgressionApplication {
 
     @Value("${http.request.read.timeout}")
