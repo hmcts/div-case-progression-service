d73a41d14722343e1dde6795360a60957cbf3a91
John O'Neill <j.oneill@kainos.com>
2018-05-29 16:54:46 +0100

    PR Comments. Created an output.tf variable for handling CNP draft store url

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -9,3 +9,7 @@ output "vaultUri" {
 output "idam_s2s_url" {
   value = "http://${var.idam_s2s_url_prefix}-${var.env}.service.${local.ase_name}.internal"
 }
+
+output "draft_store_service_url" {
+    value = "http://draft-store-service-${local.local_env}.service.core-compute-${local.local_env}.internal"
+}
