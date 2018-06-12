49e04c30e94cdd536b92acb31f789222e7d8264f
richard-diamond <richard.diamond@hmcts.net>
2018-06-12 09:56:12 +0100

    DIV-269: added new enum to contain court display names

--- src/main/java/uk/gov/hmcts/reform/divorce/transformservice/controller/CcdCallBackController.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/transformservice/controller/CcdCallBackController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,12 +20,31 @@ import uk.gov.hmcts.reform.divorce.transformservice.domain.transformservice.CCDC
 import uk.gov.hmcts.reform.divorce.transformservice.service.UpdateService;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javax.ws.rs.core.MediaType;
 
 @RestController
 @RequestMapping(path = "/caseprogression")
 @Api(value = "Transformation API", consumes = "application/json", produces = "application/json")
 public class CcdCallBackController {
+
+    private enum Courts {
+        eastMidlands("East Midlands Regional Divorce Centre"),
+        westMidlands("West Midlands Regional Divorce Centre"),
+        southWest("South West Regional Divorce Centre"),
+        northWest("North West Regional Divorce Centre");
+
+        private String displayName;
+
+        private Courts(String displayName) {
+            this.displayName = displayName;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
+    }
 
     @Autowired
     private UpdateService updateService;
@@ -45,5 +65,35 @@ public class CcdCallBackController {
 
         CoreCaseData coreCaseData = updateService.addPdf(caseDetailsRequest, authorizationToken);
         return ResponseEntity.ok(new CCDCallbackResponse(coreCaseData, new ArrayList<>(), new ArrayList<>()));
+    }
+
+    @PostMapping(path = "/petition-submitted",
+        consumes = MediaType.APPLICATION_JSON,
+        produces = MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Generate/dispatch a notification email to the petitioner when the application is submitted")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "An email notification has been generated and dispatched",
+            response = CCDCallbackResponse.class),
+        @ApiResponse(code = 400, message = "Bad Request")
+        })
+    public ResponseEntity<CCDCallbackResponse> petitionSubmitted(
+        @RequestHeader(value = "Authorization", required = false) String authorizationToken,
+        @RequestBody @ApiParam("CaseData") CreateEvent caseDetailsRequest) {
+
+        String petitionerEmail = caseDetailsRequest.getCaseDetails().getCaseData().getD8PetitionerEmail();
+
+        if (StringUtils.isNotBlank(petitionerEmail)) {
+            Map<String, String> templateVars = new HashMap<>();
+            CoreCaseData        caseData     = caseDetailsRequest.getCaseDetails().getCaseData();
+
+            templateVars.put("email address", petitionerEmail);
+            templateVars.put("first name",    caseData.getD8PetitionerFirstName());
+            templateVars.put("last name",     caseData.getD8PetitionerLastName());
+            templateVars.put("RDC name",      Courts.valueOf(caseData.getD8DivorceUnit()).getDisplayName());
+            templateVars.put("CCD reference", caseData.getD8caseReference());
+            emailService.sendSubmissionNotificationEmail(petitionerEmail, templateVars);
+        }
+
+        return ResponseEntity.ok(new CCDCallbackResponse(null, new ArrayList<>(), new ArrayList<>()));
     }
 }
