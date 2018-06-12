c7a78be74763b3c65b1a2e09289e7007f1e4b833
richard-diamond <richard.diamond@hmcts.net>
2018-06-12 09:56:12 +0100

    DIV-269: added new enum to contain court display names

--- src/main/java/uk/gov/hmcts/reform/divorce/notifications/domain/EmailTemplateNames.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/notifications/domain/EmailTemplateNames.java
@@ -2,5 +2,5 @@ package uk.gov.hmcts.reform.divorce.notifications.domain;
 
 public enum EmailTemplateNames {
     SAVE_DRAFT,
-    APPLIC_SUBMISSION       // TODO: unsure what this should be at this stage... need to talk with Neil McWhinnie
+    APPLIC_SUBMISSION
 }
