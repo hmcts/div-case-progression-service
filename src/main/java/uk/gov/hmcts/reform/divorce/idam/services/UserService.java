eaf1a1bf114b5467660981d31806d4723611f18c
Daniel Nenkov <daniel@avcora.com>
2018-06-14 12:39:01 +0100

    Added new metric when getting user details

--- src/main/java/uk/gov/hmcts/reform/divorce/idam/services/UserService.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/idam/services/UserService.java
@@ -4,19 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.idam.api.IdamApiClient;
 import uk.gov.hmcts.reform.divorce.idam.models.UserDetails;
+import uk.gov.hmcts.reform.divorce.insights.AppInsights;
 
 @Component
 public class UserService {
 
     private final IdamApiClient idamApiClient;
+    private final AppInsights appInsights;
 
     @Autowired
-    public UserService(IdamApiClient idamApiClient) {
+    public UserService(IdamApiClient idamApiClient, AppInsights appInsights) {
         this.idamApiClient = idamApiClient;
+        this.appInsights = appInsights;
     }
 
     public UserDetails getUserDetails(String authorisation) {
-        return idamApiClient.retrieveUserDetails(authorisation);
+        long detailsStart = System.currentTimeMillis();
+        UserDetails userDetails = idamApiClient.retrieveUserDetails(authorisation);
+        long detailsEnd = System.currentTimeMillis();
+
+        appInsights.trackMetric("IdAM User Details", detailsEnd - detailsStart);
+
+        return userDetails;
     }
 
 }
