31249be469c4c29df2511cbb37706d5368ed6e04
richard-diamond <richard.diamond@hmcts.net>
2018-06-14 09:30:49 +0100

    DIV-746: added code to issue an issuance email

--- src/main/java/uk/gov/hmcts/reform/divorce/transformservice/controller/CcdCallBackController.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/transformservice/controller/CcdCallBackController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.divorce.notifications.service.EmailService;
 import uk.gov.hmcts.reform.divorce.transformservice.domain.ccd.CreateEvent;
 import uk.gov.hmcts.reform.divorce.transformservice.domain.model.ccd.CoreCaseData;
 import uk.gov.hmcts.reform.divorce.transformservice.domain.transformservice.CCDCallbackResponse;
 import uk.gov.hmcts.reform.divorce.transformservice.service.UpdateService;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javax.ws.rs.core.MediaType;
 
 @RestController
@@ -25,6 +29,8 @@ import javax.ws.rs.core.MediaType;
 @Api(value = "Transformation API", consumes = "application/json", produces = "application/json")
 public class CcdCallBackController {
 
+    @Autowired
+    private EmailService emailService;
     @Autowired
     private UpdateService updateService;
 
@@ -36,11 +42,26 @@ public class CcdCallBackController {
             response = CCDCallbackResponse.class),
         @ApiResponse(code = 400, message = "Bad Request")
         })
-    public ResponseEntity<CCDCallbackResponse> addPdf(
+    public ResponseEntity<CCDCallbackResponse> petitionIssued(
         @RequestHeader(value = "Authorization", required = false) String authorizationToken,
         @RequestBody @ApiParam("CaseData") CreateEvent caseDetailsRequest) {
 
-        CoreCaseData coreCaseData = updateService.addPdf(caseDetailsRequest, authorizationToken);
+        CoreCaseData coreCaseData    = updateService.addPdf(caseDetailsRequest, authorizationToken);
+        String       petitionerEmail = caseDetailsRequest.getCaseDetails().getCaseData().getD8PetitionerEmail();
+
+        if (StringUtils.isNotBlank(petitionerEmail)) {
+            Map<String, String> templateVars = new HashMap<>();
+            CoreCaseData        caseData     = caseDetailsRequest.getCaseDetails().getCaseData();
+
+            // TODO: below is a guess of the minimum required data variables for the issuance template.
+            // Obviously the vars need to be adapted/adjusted once the template and its variables are known.
+            templateVars.put("email address", petitionerEmail);
+            templateVars.put("first name",    caseData.getD8PetitionerFirstName());
+            templateVars.put("last name",     caseData.getD8PetitionerLastName());
+            templateVars.put("CCD reference", caseData.getD8caseReference());
+            emailService.sendIssuanceNotificationEmail(petitionerEmail, templateVars);
+        }
+
         return ResponseEntity.ok(new CCDCallbackResponse(coreCaseData, new ArrayList<>(), new ArrayList<>()));
     }
 }
