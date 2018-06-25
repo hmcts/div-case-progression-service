516188df46da0d730e8d7fe93c0e8dfb4aab1051
tony.chow <tony.chow@hmcts.net>
2018-06-25 11:39:09 +0100

    Added Validation Controller endpoint and resolved latest master rebase

--- src/main/java/uk/gov/hmcts/reform/divorce/validationservice/domain/ValidationRequest.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/validationservice/domain/ValidationRequest.java
@@ -1,15 +1,24 @@
 package uk.gov.hmcts.reform.divorce.validationservice.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@ApiModel(description = "The validation request")
 public class ValidationRequest {
-
+    @ApiModelProperty(value = "The data to be validated")
     private Map<String, Object> data;
-
+    @ApiModelProperty(value = "The form the data came from")
     private String formId;
-
+    @ApiModelProperty(value = "The section of the form to be validated")
     private String sectionId;
 }
--- src/main/java/uk/gov/hmcts/reform/divorce/validationservice/domain/ValidationResponse.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/validationservice/domain/ValidationResponse.java
@@ -1,15 +1,24 @@
 package uk.gov.hmcts.reform.divorce.validationservice.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@ApiModel(description = "The response to a validation request")
 public class ValidationResponse {
-
+    @ApiModelProperty(value = "The status of the validation")
     private String validationStatus;
-
+    @ApiModelProperty(value = "The warnings returned by the validation")
     private List<String> warnings;
-
+    @ApiModelProperty(value = "The errors returned by the validation")
     private List<String> errors;
 }
