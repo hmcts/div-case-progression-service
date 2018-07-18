40f6d14a35235c423e23268d962d2fbdf386439c
Warren Taggart <warren.taggart@hmcts.net>
2018-07-18 16:01:57 +0100

    DIV-2620
    Setting explicit path

--- src/main/java/uk/gov/hmcts/reform/divorce/transformservice/controller/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/transformservice/controller/RootController.java
@@ -2,9 +2,11 @@ package uk.gov.hmcts.reform.divorce.transformservice.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/")
 public class RootController {
 
     /**
