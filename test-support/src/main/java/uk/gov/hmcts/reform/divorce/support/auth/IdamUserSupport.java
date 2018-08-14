0677cd6f93d03394fbcb9b2adb73679143eba551
Ganeshraja Ramu <ganeshraja.ramu@hmcts.net>
2018-08-14 11:26:09 +0100

    sidam changes

--- test-support/src/main/java/uk/gov/hmcts/reform/divorce/support/auth/IdamUserSupport.java
+++ test-support/src/main/java/uk/gov/hmcts/reform/divorce/support/auth/IdamUserSupport.java
@@ -4,12 +4,20 @@ import io.restassured.RestAssured;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.divorce.utils.IDAMUtils;
+import uk.gov.hmcts.reform.divorce.utils.impl.IDAMUtilsImpl;
 
 import java.util.Base64;
 import java.util.UUID;
 
 @Service
 public class IdamUserSupport {
+
+    IDAMUtils idamUtils;
+
+    public IdamUserSupport() {
+         this.idamUtils = new IDAMUtilsImpl();
+    }
 
     private static final String idamCaseworkerUser = "CaseWorkerTest";
 
@@ -30,7 +38,7 @@ public class IdamUserSupport {
         String username = "simulate-delivered" + UUID.randomUUID() + "@notifications.service.gov.uk";
         String password = UUID.randomUUID().toString();
         createUserInIdam(username, password);
-        return generateUserTokenWithNoRoles(username, password);
+        return idamUtils.generateUserTokenWithNoRoles(username, password);
     }
 
     public synchronized String getIdamTestUser() {
@@ -42,13 +50,13 @@ public class IdamUserSupport {
 
     protected void createUserAndToken() {
         createUserInIdam();
-        testUserJwtToken = generateUserTokenWithNoRoles(idamUsername, idamPassword);
+        testUserJwtToken = idamUtils.generateUserTokenWithNoRoles(idamUsername, idamPassword);
     }
 
     public synchronized String getIdamTestCaseWorkerUser() {
         if (StringUtils.isBlank(testCaseworkerJwtToken)) {
             createCaseworkerUserInIdam();
-            testCaseworkerJwtToken = generateUserTokenWithNoRoles(idamCaseworkerUser, idamCaseworkerPw);
+            testCaseworkerJwtToken = idamUtils.generateUserTokenWithNoRoles(idamCaseworkerUser, idamCaseworkerPw);
         }
 
         return testCaseworkerJwtToken;
@@ -66,7 +74,7 @@ public class IdamUserSupport {
         idamUsername = "simulate-delivered" + UUID.randomUUID() + "@notifications.service.gov.uk";
         idamPassword = UUID.randomUUID().toString();
 
-        createUserInIdam(idamUsername, idamPassword);
+        idamUtils.createUserInIdam(idamUsername, idamPassword);
     }
 
     private void createCaseworkerUserInIdam() {
@@ -83,15 +91,5 @@ public class IdamUserSupport {
         return idamUserBaseUrl + "/testing-support/accounts";
     }
 
-    private String generateUserTokenWithNoRoles(String username, String password) {
-        final String encoded = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
-        final String token = RestAssured.given().baseUri(idamUserBaseUrl)
-            .header("Authorization", "Basic " + encoded)
-            .post("/oauth2/authorize?response_type=token&client_id=divorce.support&redirect_uri="
-                + "https://www.preprod.ccd.reform.hmcts.net/oauth2redirect")
-            .body()
-            .path("access-token");
 
-        return "Bearer " + token;
-    }
 }
