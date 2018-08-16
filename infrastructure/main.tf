5b212ac3f25de693d1c5e07c8993cbb6f11997fd
David Gimelle <davidgimelle@gmail.com>
2018-08-16 16:32:14 +0100

    azure key vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -41,8 +41,8 @@ module "div-case-progression" {
         AUTH_PROVIDER_SERVICE_CLIENT_BASEURL = "${local.idam_s2s_url}"
         AUTH_SERVICE_CLIENT_DIVORCE_CCD_SUBMISSION_NAME = "${var.auth_provider_service_client_microservice_div_ccd_submission}"
         AUTH_SERVICE_CLIENT_DIVORCE_FRONTEND_NAME = "${var.auth_provider_service_client_microservice_div_frontend}"
-        AUTH_SERVICE_DIVORCE_CCD_SUBMISSION_KEY = "${data.vault_generic_secret.ccd-submission-s2s-auth-secret.data["value"]}"
-        AUTH_SERVICE_DIVORCE_FRONTEND_KEY = "${data.vault_generic_secret.frontend_secret.data["value"]}"
+        AUTH_SERVICE_DIVORCE_CCD_SUBMISSION_KEY = "${data.azurerm_key_vault_secret.ccd-submission-s2s-auth-secret.value}"
+        AUTH_SERVICE_DIVORCE_FRONTEND_KEY = "${data.azurerm_key_vault_secret.frontend_secret.value}"
         AUTH_PROVIDER_SERVICE_CLIENT_TOKENTIMETOLIVEINSECONDS = "${var.auth_provider_service_client_tokentimetoliveinseconds}"
         AUTH_PROVIDER_HEALTH_URI = "${local.idam_s2s_url}/health"
         CCD_CASEDATASTORE_BASEURL = "${local.ccd_casedatastore_baseurl}"
@@ -54,10 +54,10 @@ module "div-case-progression" {
         LOGGING_LEVEL_UK_GOV_HMCTS_CCD = "${var.logging_level_uk_gov_hmcts_ccd}"
         PDF_GENERATOR_BASE_URL = "${local.pdf_generator_base_url}"
         PDF_GENERATOR_HEALTHURL = "${local.pdf_generator_base_url}/health"
-        DRAFT_STORE_API_ENCRYPTION_KEY = "${data.vault_generic_secret.draft-store-api-encryption-key.data["value"]}"
+        DRAFT_STORE_API_ENCRYPTION_KEY = "${data.azurerm_key_vault_secret.draft-store-api-encryption-key.value}"
         DRAFT_STORE_API_BASEURL = "${local.draft_store_api_baseurl}"
         DRAFT_STORE_API_HEALTH_URI = "${local.draft_store_api_baseurl}/health"
-        UK_GOV_NOTIFY_API_KEY = "${data.vault_generic_secret.uk-gov-notify-api-key.data["value"]}"
+        UK_GOV_NOTIFY_API_KEY = "${data.azurerm_key_vault_secret.uk-gov-notify-api-key.value}"
         UK_GOV_NOTIFY_EMAIL_TEMPLATES = "${var.uk_gov_notify_email_templates}"
         UK_GOV_NOTIFY_EMAIL_TEMPLATE_VARS = "${var.uk_gov_notify_email_template_vars}"
         DOCUMENT_MANAGEMENT_STORE_URL = "${local.dm_store_url}"
@@ -86,40 +86,37 @@ module "key-vault" {
     product_group_object_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
 }
 
-data "vault_generic_secret" "ccd-submission-s2s-auth-secret" {
-    path = "secret/${var.vault_env}/ccidam/service-auth-provider/api/microservice-keys/divorceCcdSubmission"
+data "azurerm_key_vault" "div_key_vault" {
+    name                = "${local.vaultName}"
+    resource_group_name = "${local.vaultName}"
 }
 
-data "vault_generic_secret" "frontend_secret" {
-    path = "secret/${var.vault_env}/ccidam/service-auth-provider/api/microservice-keys/divorce-frontend"
-}
-
-data "vault_generic_secret" "div-doc-s2s-auth-secret" {
-    path = "secret/${var.vault_env}/ccidam/service-auth-provider/api/microservice-keys/divorceDocumentGenerator"
-}
-
-data "vault_generic_secret" "draft-store-api-encryption-key" {
-    path = "secret/${var.vault_env}/divorce/draft/encryption_key"
-}
-
-data "vault_generic_secret" "uk-gov-notify-api-key" {
-    path = "secret/${var.vault_env}/divorce/notify/api_key"
-}
-
-resource "azurerm_key_vault_secret" "ccd-submission-s2s-auth-secret" {
+data "azurerm_key_vault_secret" "ccd-submission-s2s-auth-secret" {
     name      = "ccd-submission-s2s-auth-secret"
-    value     = "${data.vault_generic_secret.ccd-submission-s2s-auth-secret.data["value"]}"
-    vault_uri = "${module.key-vault.key_vault_uri}"
+    vault_uri = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
 }
 
-resource "azurerm_key_vault_secret" "div-doc-s2s-auth-secret" {
+data "azurerm_key_vault_secret" "frontend_secret" {
+    name      = "frontend-secret"
+    vault_uri = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "ccd-submission-s2s-auth-secret" {
+    name      = "ccd-submission-s2s-auth-secret"
+    vault_uri = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "div-doc-s2s-auth-secret" {
     name      = "div-doc-s2s-auth-secret"
-    value     = "${data.vault_generic_secret.div-doc-s2s-auth-secret.data["value"]}"
-    vault_uri = "${module.key-vault.key_vault_uri}"
+    vault_uri = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
 }
 
-resource "azurerm_key_vault_secret" "draft-store-api-encryption-key" {
+data "azurerm_key_vault_secret" "draft-store-api-encryption-key" {
     name      = "draft-store-api-encryption-key"
-    value     = "${data.vault_generic_secret.draft-store-api-encryption-key.data["value"]}"
-    vault_uri = "${module.key-vault.key_vault_uri}"
+    vault_uri = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "uk-gov-notify-api-key" {
+    name      = "uk-gov-notify-api-key"
+    vault_uri = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
 }
