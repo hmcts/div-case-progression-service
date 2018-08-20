6c96a4402ce754a00aa8f181b18770d61491ff93
John O'Neill <j.oneill@kainos.com>
2018-08-20 17:18:46 +0100

    DIV-2663: Updated logic to handle multiple cases retrieved cases from CCD

--- src/main/java/uk/gov/hmcts/reform/divorce/draftservice/factory/DraftResponseFactory.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/draftservice/factory/DraftResponseFactory.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.divorce.draftservice.factory;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import uk.gov.hmcts.reform.divorce.draftservice.domain.Draft;
 import uk.gov.hmcts.reform.divorce.draftservice.domain.DraftsResponse;
 
@@ -20,9 +19,9 @@ public class DraftResponseFactory {
     private static final String CASE_DATA = "case_data";
     private static final String CASE_STATE = "state";
     private static final String ID = "id";
+    private static final String MULTIPLE_REJECTED_CASES_STATE = "MultipleRejectedCases";
 
     public static DraftsResponse buildDraftResponseFromDraft(Draft draft) {
-        //check if draft is null
         if (draft == null) {
             return DraftsResponse.emptyResponse();
         } else {
@@ -36,30 +35,94 @@ public class DraftResponseFactory {
 
     public static DraftsResponse buildDraftResponseFromCaseData(List<Map<String, Object>> listOfCasesInCCD) {
 
-        if (CollectionUtils.isEmpty(listOfCasesInCCD)) {
-            log.debug("No case found to build draft response");
-            return DraftsResponse.emptyResponse();
-        }
+            /*
+            - Only 1 case is not "Rejected"
+             Apply the existing resume logic as per DIV-2658 
+            return draftResponseBuilder(listOfCasesInCCD);
+
+            - Multiple cases are not "Rejected" 
+            Display new page at /contact-divorce-team 
+
+            - Multiple cases are all "Rejected" 
+            Start a blank application
+             */
+
+        int numberOfPetRejectedCases = 0;
+        int numberOfPetNotRejectedCases = 0;
 
         if (listOfCasesInCCD.size() > 1) {
-            log.info("Multiple cases found. Building empty draft response");
+
+            // get count for # of rejected & non-rejected cases
+            for (Map<String, Object> caseDetails : listOfCasesInCCD) {
+                String caseState = (String) caseDetails.get(CASE_STATE);
+
+                if (caseState == "Rejected") {
+                    numberOfPetRejectedCases += 1;
+                }
+                else{
+                    numberOfPetNotRejectedCases += 1;
+                }
+            }
+            // replace above with java stream for efficiency
+
+
+            // if only 1 case is not "Rejected" -  Apply the existing resume logic as per DIV-2658 
+            if (numberOfPetNotRejectedCases == 1) {
+
+                for (int i = 0; i < listOfCasesInCCD.size(); i++) {
+                    Map<String, Object> caseDetails = listOfCasesInCCD.get(i);
+                    String caseState = (String) caseDetails.get(CASE_STATE);
+
+                    if (caseState == "Rejected") {
+                        log.info("Multiple cases found - only 1 is not rejected");
+                        return draftResponseBuilder((List<Map<String, Object>>) listOfCasesInCCD.get(i));
+                    }
+                }
+            }
+
+            // if multiple cases are not "Rejected"  - Display new page at /contact-divorce-team 
+            if (numberOfPetNotRejectedCases > 1) {
+
+                log.info("Multiple cases found - Multiple are not rejected");
+                return draftResponseBuilder(listOfCasesInCCD, MULTIPLE_REJECTED_CASES_STATE);
+            }
+
+            //if multiple cases are all "Rejected"  - Start a blank application
+            if (numberOfPetRejectedCases == listOfCasesInCCD.size()) {
+
+                log.info("Multiple cases found - all are rejected");
+                return DraftsResponse.emptyResponse();
+            }
+
+            log.info("Unhandled situation with multiple cases. Building empty draft response");
             return DraftsResponse.emptyResponse();
         }
 
-        Map<String, Object> caseDetails = listOfCasesInCCD.get(0);
+        return draftResponseBuilder(listOfCasesInCCD);
+    }
 
+    private static DraftsResponse draftResponseBuilder(List<Map<String, Object>> listOfCasesInCCD, String customState){
+
+        Map<String, Object> caseDetails = listOfCasesInCCD.get(0);
         log.debug("Building draft response from existing case {} in CCD", caseDetails.get(ID));
+
+        String caseState = (customState == null) ? (String) caseDetails.get(CASE_STATE) : customState;
 
         ObjectNode jsonNode = new ObjectNode(JsonNodeFactory.instance);
         jsonNode.put(CASE_ID, (Long) caseDetails.get(ID));
         Map<String, Object> caseData = (Map<String, Object>) caseDetails.get(CASE_DATA);
         jsonNode.put(COURTS, (String) caseData.get(D_8_DIVORCE_UNIT));
         jsonNode.put(SUBMISSION_STARTED, true);
-        jsonNode.put(CASE_STATE, (String) caseDetails.get(CASE_STATE));
+        jsonNode.put(CASE_STATE, caseState);
 
         return DraftsResponse.builder()
-                .isDraft(false)
-                .data(jsonNode)
-                .build();
+            .isDraft(false)
+            .data(jsonNode)
+            .build();
+    }
+
+    private static DraftsResponse draftResponseBuilder(List<Map<String, Object>> listOfCasesInCCD) {
+
+        return draftResponseBuilder(listOfCasesInCCD, null);
     }
 }
