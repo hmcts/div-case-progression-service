31c908d9b1c7b0d136b8f02646041ace06d474f9
David Gimelle <davidgimelle@gmail.com>
2018-09-12 16:53:16 +0100

    Add CcdToPaymentMapper

---
+++ src/main/java/uk/gov/hmcts/reform/divorce/transformservice/mapping/CcdToPaymentMapper.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.divorce.transformservice.mapping;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.divorce.transformservice.domain.model.divorceapplicationdata.Payment;
+
+import java.util.*;
+
+@Slf4j
+@Component
+public class CcdToPaymentMapper {
+
+    public List<Payment> ccdToPaymentRef(Map<String, Object> caseData) {
+        log.info("Payment record from CCD getPayments {}", caseData.get("Payments"));
+        List<Payment> paymentList = Collections.emptyList();
+        if (caseData.get("Payments") != null) {
+            JsonNode payments = (JsonNode) caseData.get("Payments");
+            if (payments.isArray()) {
+                paymentList = new ArrayList<>();
+                for (JsonNode node : payments) {
+                    JsonNode value = node.get("value");
+                    Payment payment = new Payment();
+                    payment.setPaymentReference(value.get("PaymentReference").textValue());
+                    payment.setPaymentStatus(value.get("PaymentStatus").textValue());
+                    paymentList.add(payment);
+                }
+            }
+        }
+        return paymentList;
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/divorce/transformservice/mapping/CcdToPaymentMapperTest.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.divorce.transformservice.mapping;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.divorce.support.util.ResourceLoader;
+import uk.gov.hmcts.reform.divorce.transformservice.domain.model.divorceapplicationdata.Payment;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CcdToPaymentMapperTest {
+
+    @InjectMocks
+    private CcdToPaymentMapper mapper;
+
+    @Test
+    public void mapperMapFromAList() throws Exception {
+
+        Map<String, Object> ccdPayments = getPaymentsAsCaseDataMap();
+
+        final List<Payment> paymentList = mapper.ccdToPaymentRef(ccdPayments);
+
+        assertThat(paymentList.size()).isEqualTo(3);
+        assertThat(paymentList.get(0).getPaymentReference()).isEqualTo("RC-1536-5783-3942-9827");
+        assertThat(paymentList.get(0).getPaymentStatus()).isEqualTo("Success");
+        assertThat(paymentList.get(1).getPaymentStatus()).isEqualTo("Initiated");
+    }
+
+    private Map<String, Object> getPaymentsAsCaseDataMap() throws Exception {
+        String paymentsAsString = ResourceLoader.loadAsText("divorce-payload-json/3PaymentsCcd.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<HashMap<String, JsonNode>> typeRef = new TypeReference<HashMap<String, JsonNode>>() {};
+        return objectMapper.readValue(paymentsAsString, typeRef);
+    }
+}
