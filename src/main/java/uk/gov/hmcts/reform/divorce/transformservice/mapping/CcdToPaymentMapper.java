b0b836b47130d789f0872671f910d29bd3b0be28
David Gimelle <davidgimelle@gmail.com>
2018-09-12 16:08:26 +0100

    Add unit test

--- src/main/java/uk/gov/hmcts/reform/divorce/transformservice/mapping/CcdToPaymentMapper.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/transformservice/mapping/CcdToPaymentMapper.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.divorce.transformservice.mapping;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.transformservice.domain.model.divorceapplicationdata.Payment;
@@ -17,23 +18,18 @@ public class CcdToPaymentMapper {
         log.info("Payment record from CCD getPayments {}", caseData.get("Payments"));
         List<Payment> paymentList = Collections.emptyList();
         if (caseData.get("Payments") != null) {
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            try {
-                JsonNode payments = objectMapper.readTree((String) caseData.get("Payments"));
-                log.info("Payment record from CCD Payments {}", payments);
-                if (payments.isArray()) {
-                    paymentList = new ArrayList<>();
-                    for (JsonNode node : payments) {
-                        JsonNode value = node.get("value");
-                        log.info("Payment record from CCD value {}", value);
-                        Payment payment = new Payment();
-                        payment.setPaymentReference(value.get("PaymentReference").textValue());
-                        payment.setPaymentStatus(value.get("PaymentStatus").textValue());
-                    }
+            JsonNode payments = (JsonNode) caseData.get("Payments");
+            log.info("Payment record from CCD Payments {}", payments);
+            if (payments.isArray()) {
+                paymentList = new ArrayList<>();
+                for (JsonNode node : payments) {
+                    JsonNode value = node.get("value");
+                    log.info("Payment record from CCD value {}", value);
+                    Payment payment = new Payment();
+                    payment.setPaymentReference(value.get("PaymentReference").textValue());
+                    payment.setPaymentStatus(value.get("PaymentStatus").textValue());
+                    paymentList.add(payment);
                 }
-            } catch (IOException ioe) {
-                throw new RuntimeException("Error mapping payment", ioe);
             }
         }
         return paymentList;
--- src/test/java/uk/gov/hmcts/reform/divorce/transformservice/mapping/CcdToPaymentMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/transformservice/mapping/CcdToPaymentMapperTest.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.divorce.transformservice.mapping;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,7 +17,11 @@ import uk.gov.hmcts.reform.divorce.transformservice.domain.model.ccd.Document;
 import uk.gov.hmcts.reform.divorce.transformservice.domain.model.divorceapplicationdata.Payment;
 import uk.gov.hmcts.reform.divorce.transformservice.domain.pdf.PdfFile;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,21 +32,27 @@ public class CcdToPaymentMapperTest {
     @InjectMocks
     private CcdToPaymentMapper mapper;
 
-    @Ignore
+
     @Test
     public void mapperMapFromAList() throws Exception {
 
         //List of payment from a file as a string
         String paymentsAsString = ResourceLoader.loadAsText("divorce-payload-json/3PaymentsCcd.json");
 
-        //map it
-      /*  = mapper.ccdToPaymentsMap(paymentsAsString);
+        ObjectMapper objectMapper = new ObjectMapper();
 
+        TypeReference<HashMap<String,JsonNode >> typeRef
+            = new TypeReference<HashMap<String, JsonNode>>() {};
+
+        Map<String, Object> caseData = objectMapper.readValue(paymentsAsString, typeRef);
+
+        //map it
+        final List<Payment> paymentList = mapper.ccdTpPaymentRef(caseData);
         //check list size
-        assertThat(payments.size()).isEqualTo(3);
+        assertThat(paymentList.size()).isEqualTo(3);
         //check first payment is success
-        assertThat(payments.get(0).getPaymentReference()).isEqualTo("RC-1536-5783-3942-9827");
-        assertThat(payments.get(0).getPaymentStatus()).isEqualTo("Success");
-        assertThat(payments.get(1).getPaymentStatus()).isEqualTo("Initiated");*/
+        assertThat(paymentList.get(0).getPaymentReference()).isEqualTo("RC-1536-5783-3942-9827");
+        assertThat(paymentList.get(0).getPaymentStatus()).isEqualTo("Success");
+        assertThat(paymentList.get(1).getPaymentStatus()).isEqualTo("Initiated");
     }
 }
