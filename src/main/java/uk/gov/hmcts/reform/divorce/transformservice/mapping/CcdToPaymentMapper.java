16ffebbb919e05718d4ac875f1c42952573a5d08
David Gimelle <davidgimelle@gmail.com>
2018-09-13 15:01:35 +0100

    Clean Code

--- src/main/java/uk/gov/hmcts/reform/divorce/transformservice/mapping/CcdToPaymentMapper.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/transformservice/mapping/CcdToPaymentMapper.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.divorce.transformservice.mapping;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.transformservice.domain.model.divorceapplicationdata.Payment;
@@ -17,23 +18,21 @@ public class CcdToPaymentMapper {
 
     public List<Payment> ccdToPaymentRef(Map<String, Object> caseData) {
         log.info("Payment record from CCD getPayments {}", caseData.get("Payments"));
-        if (caseData.get("Payments") !=null) {
+        if (caseData.get("Payments") != null) {
             log.info("Payment record from CCD getPayments class {}",
                 caseData.get("Payments").getClass().getCanonicalName());
         }
         List<Payment> paymentList = Collections.emptyList();
 
         if (caseData.get("Payments") != null) {
-            JsonNode payments = (JsonNode) caseData.get("Payments");
-            if (payments.isArray()) {
-                paymentList = new ArrayList<>();
-                for (JsonNode node : payments) {
-                    JsonNode value = node.get("value");
-                    Payment payment = new Payment();
-                    payment.setPaymentReference(value.get("PaymentReference").textValue());
-                    payment.setPaymentStatus(value.get("PaymentStatus").textValue());
-                    paymentList.add(payment);
-                }
+            ArrayNode payments = (ArrayNode) caseData.get("Payments");
+            paymentList = new ArrayList<>();
+            for (JsonNode node : payments) {
+                JsonNode value = node.get("value");
+                Payment payment = new Payment();
+                payment.setPaymentReference(value.get("PaymentReference").textValue());
+                payment.setPaymentStatus(value.get("PaymentStatus").textValue());
+                paymentList.add(payment);
             }
         }
 
