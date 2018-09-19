bd6ef2d15f30af2aa85583449923415afc09274d
David Gimelle <davidgimelle@gmail.com>
2018-09-19 10:46:18 +0100

    fix after merging

--- test-support/src/main/java/uk/gov/hmcts/reform/divorce/support/caseprogression/BaseIntegrationTest.java
+++ test-support/src/main/java/uk/gov/hmcts/reform/divorce/support/caseprogression/BaseIntegrationTest.java
@@ -124,7 +124,7 @@ public abstract class BaseIntegrationTest extends BaseIntegrationTestWithIdamSup
     public Response submitEvent(long caseId, String eventId) throws Exception {
         String eventToken = createEventAsCaseWorker(caseId, eventId);
 
-        JSONObject jsonObject = new JSONObject(loadJSON("submit-event.json"));
+        JSONObject jsonObject = new JSONObject(loadJson("submit-event.json"));
         JSONObject eventObject = jsonObject.getJSONObject("event").put("id", eventId);
         jsonObject.put("event", eventObject);
         jsonObject.put("event_token", eventToken);
